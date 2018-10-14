ec40c1afed3493f0924dcb839cd118bbbf50ce09
Andreas Teneketzoglou <antekai00@gmail.com>
2018-10-14 14:48:43 +0200

    add basic layout style

--- src/Map/Screen.js
+++ src/Map/Screen.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { MapLocationList } from "./LocationList";
 import { GoogleMap } from "./GoogleMap";
-
+import { Button } from "antd";
+import "./Screen.css";
 class MapScreen extends React.Component {
   render() {
     return (
-      <div>
-        <GoogleMap />
-        <MapLocationList />
+      <div className="flexContainer">
+        <div className="flexItem">
+          <GoogleMap />
+        </div>
+
+        <div className="flexItem">
+          <Button type="primary">Add Location</Button>
+          <Button type="danger">Reset map</Button>
+          <MapLocationList />
+        </div>
       </div>
     );
   }
