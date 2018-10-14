7c904fa52155000859786019207b1f7b4c1d4afc
Andreas Teneketzoglou <antekai00@gmail.com>
2018-10-14 14:28:07 +0200

    ignore helper files for test coverage

--- wallaby.js
+++ wallaby.js
@@ -1,41 +1,47 @@
-module.exports = function (wallaby) {
+module.exports = function(wallaby) {
+  // Babel, jest-cli and some other modules may be located under
+  // react-scripts/node_modules, so need to let node.js know about it
+  var path = require("path");
+  process.env.NODE_PATH +=
+    path.delimiter +
+    path.join(__dirname, "node_modules") +
+    path.delimiter +
+    path.join(__dirname, "node_modules/react-scripts/node_modules");
+  require("module").Module._initPaths();
 
-    // Babel, jest-cli and some other modules may be located under
-    // react-scripts/node_modules, so need to let node.js know about it
-    var path = require('path');
-    process.env.NODE_PATH +=
-        path.delimiter +
-        path.join(__dirname, 'node_modules') +
-        path.delimiter +
-        path.join(__dirname, 'node_modules/react-scripts/node_modules');
-    require('module').Module._initPaths();
+  return {
+    files: [
+      "src/**/*.+(js|jsx|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)",
+      "!src/**/*.test.js?(x)",
+      { pattern: "src/index.js", instrument: false },
+      { pattern: "src/serviceWorker.js", instrument: false }
+    ],
+    filesWithNoCoverageCalculated: ["src/index.js", "src/serviceWorker.js"],
 
-    return {
-        files: [
-            'src/**/*.+(js|jsx|json|snap|css|less|sass|scss|jpg|jpeg|gif|png|svg)',
-            '!src/**/*.test.js?(x)'
-        ],
+    tests: ["src/**/*.test.js?(x)"],
 
-        tests: ['src/**/*.test.js?(x)'],
+    env: {
+      type: "node",
+      runner: "node"
+    },
 
-        env: {
-            type: 'node',
-            runner: 'node'
-        },
+    compilers: {
+      "**/*.js?(x)": wallaby.compilers.babel({
+        presets: ["react-app"]
+      })
+    },
 
-        compilers: {
-            '**/*.js?(x)': wallaby.compilers.babel({
-                presets: ['react-app']
-            })
-        },
+    setup: wallaby => {
+      const jestConfig = require("react-scripts/scripts/utils/createJestConfig")(
+        p => require.resolve("react-scripts/" + p)
+      );
+      Object.keys(jestConfig.transform || {}).forEach(
+        k => ~k.indexOf("^.+\\.(js|jsx") && void delete jestConfig.transform[k]
+      );
+      delete jestConfig.testEnvironment;
+      wallaby.testFramework.configure(jestConfig);
+    },
 
-        setup: wallaby => {
-            const jestConfig = require('react-scripts/scripts/utils/createJestConfig')(p => require.resolve('react-scripts/' + p));
-            Object.keys(jestConfig.transform || {}).forEach(k => ~k.indexOf('^.+\\.(js|jsx') && void delete jestConfig.transform[k]);
-            delete jestConfig.testEnvironment;
-            wallaby.testFramework.configure(jestConfig);
-        },
-
-        testFramework: 'jest'
-    };
+    testFramework: "jest"
+  };
 };
