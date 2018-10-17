d8cdd5f73d84d6e25e407cdcee9336e58036acf0
Andreas Teneketzoglou <antekai00@gmail.com>
2018-10-17 20:26:21 +0200

    feat(addBundleAnalyzer): analyze bundle with webpack-bundle-analyzer

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,18 @@
  const { injectBabelPlugin } = require('react-app-rewired');
+ const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer')
 
   module.exports = function override(config, env) {
-   config = injectBabelPlugin(
+  //  inject babel plugin for ant-design
+    config = injectBabelPlugin(
      ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
      config,
    );
+   // config for webpack analyzer
+   if (env === 'production') {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: 'static',
+      reportFilename: 'report.html'
+    })
+  }
     return config;
   };
---
+++ triggerBundleAnalyzer.js
@@ -0,0 +1,16 @@
+// node triggerBundleAnalyzer.js
+// creates bundle analysis:  ~/build/report.html
+process.env.NODE_ENV = "production";
+var BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
+
+const webpackConfigProd = require("react-scripts/config/webpack.config.prod");
+
+webpackConfigProd.plugins.push(
+  new BundleAnalyzerPlugin({
+    analyzerMode: "static",
+    reportFilename: "report.html"
+  })
+);
+
+require("react-scripts/scripts/build");
